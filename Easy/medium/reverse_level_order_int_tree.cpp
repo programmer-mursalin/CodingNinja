6960df22006372876172ddcd3160d2698d9cfb17
Mursalin Hawlader <programmer.mursalin016@gmail.com>
2023-10-03 00:35:44 +0600

    Create reverse_level_order_int_tree.cpp

---
+++ Easy/medium/reverse_level_order_int_tree.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+vector<int> reverseLevelOrder(TreeNode<int> *root)
+{
+    vector<int> v;
+    queue<TreeNode<int> *> q;
+    if (root)
+        q.push(root);
+    while (!q.empty())
+    {
+        TreeNode<int> *pr = q.front();
+        q.pop();
+        v.push_back(pr->val);
+        if (pr->left)
+            q.push(pr->left);
+        if (pr->right)
+            q.push(pr->right);
+    }
+    reverse(v.begin(), v.end());
+    return v;
+}
