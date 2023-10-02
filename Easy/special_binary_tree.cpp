c61a9287a129f374fe8a53cbd934d86144852cd9
Mursalin Hawlader <programmer.mursalin016@gmail.com>
2023-10-02 11:28:14 +0600

    Create special_binary_tree.cpp

---
+++ Easy/special_binary_tree.cpp
@@ -0,0 +1,12 @@
+bool isSpecialBinaryTree(BinaryTreeNode<int> *root)
+{
+    if (root->left == NULL && root->right == NULL)
+        return true;
+    if (root->left == NULL || root->right == NULL)
+        return false;
+    bool l = isSpecialBinaryTree(root->left);
+    bool r = isSpecialBinaryTree(root->right);
+    if (l == false || r == false)
+        return false;
+    return true;
+}
